ac494d8018602eb28bdc257c730284dcf2379645
Jimesarrice <38239619+Jimesarrice@users.noreply.github.com>
2018-04-10 14:53:46 +0800

    Add files via upload

---
+++ DBSelect/DBselect.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DBSelect
+{
+    partial class DBselect
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.resout = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(12, 11);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(389, 21);
+            this.Username.TabIndex = 1;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(12, 38);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(389, 21);
+            this.Password.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "注册";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resout
+            // 
+            this.resout.Location = new System.Drawing.Point(12, 67);
+            this.resout.Name = "resout";
+            this.resout.Size = new System.Drawing.Size(470, 222);
+            this.resout.TabIndex = 5;
+            this.resout.Text = "";
+            // 
+            // DBselect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 303);
+            this.Controls.Add(this.resout);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Name = "DBselect";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox resout;
+    }
+}
+
---
+++ DBSelect/DBselect.cs
@@ -0,0 +1,190 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBSelect
+{
+    public partial class DBselect : Form
+    {
+        public DBselect()
+        {
+            InitializeComponent();
+            mc.SetServer("localhost")
+            .SetDataBase("mysql")
+            .SetUserID("root")
+            .SetPassword("Xkn99777")
+            .SetDataBase("projectevedb")
+            .SetPort("3306")
+            .SetCharset("utf8");
+        }
+
+        public class MysqlConnector
+        {
+            string server = null;
+            string userid = null;
+            string password = null;
+            string database = null;
+            string port = "3306";
+            string charset = "utf-8";
+
+            public MysqlConnector() { }
+            public MysqlConnector SetServer(string server)
+            {
+                this.server = server;
+                return this;
+            }
+            public MysqlConnector SetUserID(string userid)
+            {
+                this.userid = userid;
+                return this;
+            }
+            public MysqlConnector SetDataBase(string database)
+            {
+                this.database = database;
+                return this;
+            }
+            public MysqlConnector SetPassword(string password)
+            {
+                this.password = password;
+                return this;
+            }
+            public MysqlConnector SetPort(string port)
+            {
+                this.port = port;
+                return this;
+            }
+            public MysqlConnector SetCharset(string charset)
+            {
+                this.charset = charset;
+                return this;
+            }
+            
+            #region  建立MySql数据库连接
+            /// <summary>
+            /// 建立数据库连接.
+            /// </summary>
+            /// <returns>返回MySqlConnection对象</returns>
+            private MySqlConnection GetMysqlConnection()
+            {
+                string M_str_sqlcon = string.Format("server={0};user id={1};password={2};database={3};port={4};Charset={5}", server, userid, password, database, port, charset);
+                MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+                return myCon;
+            }
+            #endregion
+
+            #region  执行MySqlCommand命令
+            /// <summary>
+            /// 执行MySqlCommand
+            /// </summary>
+            /// <param name="M_str_sqlstr">SQL语句</param>
+            public void ExeUpdate(string M_str_sqlstr)
+            {
+                MySqlConnection mysqlcon = this.GetMysqlConnection();
+                mysqlcon.Open();
+                MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+                mysqlcom.ExecuteNonQuery();
+                mysqlcom.Dispose();
+                mysqlcon.Close();
+                mysqlcon.Dispose();
+            }
+            #endregion
+
+            #region  创建MySqlDataReader对象
+            /// <summary>
+            /// 创建一个MySqlDataReader对象
+            /// </summary>
+            /// <param name="M_str_sqlstr">SQL语句</param>
+            /// <returns>返回MySqlDataReader对象</returns>
+            public MySqlDataReader ExeQuery(string M_str_sqlstr)
+            {
+                Console.WriteLine(M_str_sqlstr);
+                MySqlConnection mysqlcon = this.GetMysqlConnection();
+                MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+                mysqlcon.Open();
+                MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+                return mysqlread;
+            }
+            #endregion
+        }
+        MysqlConnector mc = new MysqlConnector();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = string.Empty;
+                if (Username.Text == string.Empty&&Password.Text == string.Empty )
+                {
+                    MessageBox.Show("User Name or Passworld Should't be Empty", "Error", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    sql = "select PassWord,Level from UserLogin where UserName=\"" + Username.Text+"\"";
+                    //string sql = textBox1.Text;
+                    resout.Text += sql + "\r\n";
+                    string result = "";
+                    string lV = "";
+                    //执行查询
+                    MySqlDataReader reader = mc.ExeQuery(sql);
+                    if (reader.FieldCount != 2)
+                    {
+                        MessageBox.Show("User Name or Passworld is Not Correct", "Error", MessageBoxButtons.OK);
+                    }
+                    while (reader.Read())
+                    {
+                        //for (int i = 0; i < reader.FieldCount; i++)
+                        //{
+                        //    result += reader.GetName(i) + "\t" + reader.GetValue(i) + "\r\n";
+                        //}
+                        result = reader.GetValue(0).ToString();
+                        lV = reader.GetValue(1).ToString();
+                    }
+                    if (result == Password.Text)
+                    {
+                        resout.Text += "User Name or Passworld is Correct" + "\r\n";
+                        resout.Text += "用户等级：" + lV + "\r\n";
+                    }
+                    else
+                    {
+                        MessageBox.Show("User Name or Passworld is Not Correct", "Error", MessageBoxButtons.OK);
+                    }
+                }
+                //resout.Text = result;
+                //执行增删改等操作
+                //mc.ExeUpdate(sql);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            string sql = string.Empty;
+            if (Username.Text == string.Empty && Password.Text == string.Empty)
+            {
+                MessageBox.Show("User Name or Passworld Should't be Empty", "Error", MessageBoxButtons.OK);
+            }
+            else
+            {
+                sql = "INSERT INTO UserLogin (UserName,PassWord,Level) VALUES ('"+ Username.Text + "','"+ Password.Text +"',0)";
+                resout.Text += sql + "\r\n";
+                mc.ExeUpdate(sql);
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
